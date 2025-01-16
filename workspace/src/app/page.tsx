66155c3bfa287f6b6cc104091aa31e38ce06724a
gamarcad <marcadet.gael@gmail.com>
2025-01-16 14:14:02 +0100

    Remove deprecated files and implement feature updates
    Removed obsolete Dockerfile, middleware, and login-related files. Updated various components with enhanced state management and API handling. Improved user authentication redirection and context. Updated documentation for deployment with new environment configurations.

--- workspace/src/app/page.tsx
+++ workspace/src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import CarmentisSignIn from '@/components/carmentis/signIn';
+import { Typography } from '@material-tailwind/react';
+import FlexCenter from '@/components/flex-center.component';
 
 export default function Login() {
     return <section className="bg-gray-50 dark:bg-gray-900">
@@ -12,12 +14,8 @@ export default function Login() {
             <div
                 className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your wallet
-                    </h1>
-                    <div className={"flex flex-col justify-content-center items-center"}>
-                        <CarmentisSignIn></CarmentisSignIn>
-                    </div>
+                    <Typography variant={"h4"}>Sign in to your workspace</Typography>
+                    <FlexCenter children={<CarmentisSignIn/>} className={"flex-col"}/>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                         Don’t have an account yet? <a href="#"
                                                       className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
--- workspace/src/components/flex-center.component.tsx
+++ workspace/src/components/flex-center.component.tsx
@@ -1,7 +1,25 @@
 import { PropsWithChildren } from 'react';
 
-export default function FlexCenter({children}: PropsWithChildren) {
-	return <div className={"w-full h-full flex justify-center items-center align-middle"}>
+/**
+ * Interface representing the properties for a FlexCenter component.
+ *
+ * @interface
+ * @property {string} [className] - An optional string to specify the CSS class for the component, allowing for custom styling.
+ */
+export interface FlexCenterProps {
+	className?: string
+}
+/**
+ * A functional component that centers its children both vertically and horizontally
+ * using flexbox styling.
+ *
+ * @param {Object} props - The component properties.
+ * @param {React.ReactNode} props.children - The child elements to render within the component.
+ * @param {string} [props.className] - Optional additional CSS classes to apply to the component.
+ * @return {JSX.Element} A flexbox container wrapping the content.
+ */
+export default function FlexCenter({children, className}: PropsWithChildren<FlexCenterProps>) {
+	return <div className={`w-full h-full flex justify-center items-center align-middle ${className}`}>
 		{children}
 	</div>
 }
