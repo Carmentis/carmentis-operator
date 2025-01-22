a73448f5d108db827458e776824d041d5b011135
gamarcad <marcadet.gael@gmail.com>
2025-01-22 17:24:51 +0100

    Remove unused controller and add GitHub workflow for Docker
    The `OperatorApiController` has been deleted as it is no longer in use, and references to it in the module were cleaned up. A new GitHub Actions workflow was added to automate building and publishing Docker images for the operator's back and front services.

--- operator/src/operator-api/operator-api.module.ts
+++ operator/src/operator-api/operator-api.module.ts
@@ -1,12 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import PackageConfigService from '../package.service';
 import { OperatorApiGateway } from './operator-api.gateway';
-import { OperatorApiController } from './operator-api.controller';
 import { CorsMiddleware } from './cors-middleware';
 
 @Module({
 	imports: [],
-	controllers: [OperatorApiController],
+	controllers: [],
 	providers: [PackageConfigService, OperatorApiGateway],
 })
 export class OperatorApiModule {
