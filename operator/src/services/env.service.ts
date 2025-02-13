e973eab6646adf036a032661d3fa6cb62d99ea40
gamarcad <marcadet.gael@gmail.com>
2025-02-13 16:35:23 +0100

    Add logging to EnvService for operator keypair file status
    Introduce a logger to EnvService to provide feedback on the status of the operator keypair file. This improves visibility into the configuration by logging whether the file is provided or missing.

--- operator/src/services/env.service.ts
+++ operator/src/services/env.service.ts
@@ -1,20 +1,25 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EnvService implements OnModuleInit {
 	operatorKeyPairFile?: string;
 	nodeUrl : string
+	logger = new Logger(EnvService.name);
 
 	constructor(
 		private readonly configService: ConfigService
 	) {
 		this.nodeUrl = this.configService.getOrThrow<string>('NODE_URL');
+		this.operatorKeyPairFile = this.configService.get<string>('OPERATOR_KEYPAIR_FILE')
+		if (this.operatorKeyPairFile) {
+			this.logger.log(`No operator keypair file provided (${this.operatorKeyPairFile}).`)
+		} else {
+			this.logger.log(`Operator keypair file provided: ${this.operatorKeyPairFile}`)
+		}
 	}
 
 	onModuleInit(): any {
-		// load the (optional) issuer key pair file
-		this.operatorKeyPairFile = process.env.OPERATOR_KEYPAIR_FILE;
 	}
 
 }
