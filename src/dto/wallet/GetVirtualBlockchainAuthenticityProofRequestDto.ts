528103830cbe9ebf5ffec337980e18af161018fe
Gaël Marcadet <gamarcadet@gmail.com>
2026-08-13 15:11:15 +0200

    chore(dto): update examples and descriptions in `GetVirtualBlockchainAuthenticityProofRequestDto`

--- src/dto/wallet/GetVirtualBlockchainAuthenticityProofRequestDto.ts
+++ src/dto/wallet/GetVirtualBlockchainAuthenticityProofRequestDto.ts
@@ -3,17 +3,17 @@ import { IsInt, IsNumber, IsOptional, IsString } from 'class-validator';
 
 
 export class GetAuthenticityProofRequestDto {
-	@ApiProperty({ description: 'Optional author identifier for proof filtering', example: 'author_789', required: false })
+	@ApiProperty({ description: 'Optional author identifier for proof filtering', example: 'Proof Author', required: false })
 	@IsOptional()
 	@IsString()
 	proofAuthor: string
 }
 export class GetVirtualBlockchainAuthenticityProofRequestDto {
-	@ApiProperty({ description: 'The virtual blockchain identifier', example: 'vb_12345' })
+	@ApiProperty({ description: 'The virtual blockchain identifier', example: 'A209BBB06FE1895E66E6624B86E59D9B4E8798119936A79917AE744BC709590A' })
 	@IsString()
 	virtualBlockchainId: string;
 
-	@ApiProperty({ description: 'Optional author identifier for proof filtering', example: 'author_789', required: false })
+	@ApiProperty({ description: 'Optional author identifier for proof filtering', example: 'Proof Author', required: false })
 	@IsOptional()
 	@IsString()
 	proofAuthor: string
